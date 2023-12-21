31b925424c1d03e7b034593311d8d62da93409c6
EternalWill43 <haler43@icloud.com>
2023-12-21 03:15:15 -0500

    fix: valid palindrome

--- two_pointers/valid_palindrome.cpp
+++ two_pointers/valid_palindrome.cpp
@@ -24,6 +24,11 @@ public:
             {
                 return false;
             }
+            else
+            {
+                l++;
+                r--;
+            }
         }
         return true;
     }
